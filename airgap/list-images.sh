1e66c950c1ee38464c5fb5c26eabe60bce1c0d55
Zachariah Dzielinski <dzielinski_zachariah@bah.com>
2020-12-04 22:41:16 -0500

    CI test

--- airgap/list-images.sh
+++ airgap/list-images.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # obtain all unique container / init container image paths
-kubectl get pods -A -o jsonpath="{.items[*].spec['containers','initContainers'][*].image}" | tr -s '[[:space:]]' '\n' | sort | uniq -c | awk '{ print $2 }'
+# TODO - Make sure this is all the images
+kubectl get all -A -o jsonpath="{.items[*].spec['containers','initContainers'][*].image}" | tr -s '[[:space:]]' '\n' | sort | uniq -c | awk '{ print $2 }'
