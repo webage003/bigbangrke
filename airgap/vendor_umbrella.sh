88c9794d2aa204e309d685f32a19d9d5ddad07c8
Zachariah Dzielinski <dzielinski_zachariah@bah.com>
2020-12-07 09:28:31 -0500

    CI test

--- airgap/vendor_umbrella.sh
+++ airgap/vendor_umbrella.sh
@@ -5,11 +5,9 @@ repo_path=$1
 
 # vendor the umbrella repository
 echo "Vendoring the umbrella repository"
-UMBRELLA_URL=$(git config --get remote.origin.url)
-UMBRELLA_HEAD=$(git rev-parse HEAD)
 echo "Repo Path: $repo_path"
-echo "Umbrella URL: $UMBRELLA_URL"
-echo "Umbrella Head: $UMBRELLA_HEAD"
+echo "Repository URL: $CI_REPOSITORY_URL"
+echo "Commit Ref Name: $CI_COMMIT_REF_NAME"
 rm -rf $repo_path
 git clone $UMBRELLA_URL $repo_path
 cd $repo_path
