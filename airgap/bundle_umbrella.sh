dbd2eac362448129d9f8b6ea1c40c9bcf44e5a57
Zachariah Dzielinski <dzielinski_zachariah@bah.com>
2020-12-07 22:31:42 -0500

    CI test

--- airgap/bundle_umbrella.sh
+++ airgap/bundle_umbrella.sh
@@ -1,25 +1,24 @@
 #!/usr/bin/env bash
 
-set -x
-
 # repo path argument
 repo_path=$1
 repo_name=$(basename $repo_path)
+repo_url=$(git config --get remote.origin.url)
 
 # vendor the umbrella repository
 echo "Bundling the $repo_name repository"
 echo "Repo Path: $repo_path"
 echo "Repo Name: $repo_name"
-echo "Repository URL: $CI_REPOSITORY_URL"
+echo "Repository URL: $repo_url"
 echo "Commit Ref Name: $CI_COMMIT_REF_NAME"
 
 echo "Cleaning existing path $repo_path"
 rm -rf $repo_path
-echo "Cloning $CI_REPOSITORY_URL to $repo_path"
-git clone $UMBRELLA_URL $repo_path
+echo "Cloning $repo_url to $repo_path"
+git clone $repo_url $repo_path
 cd $repo_path
-echo "Bundling repository for $CI_COMMIT_REF_NAME"
-git bundle create ../$repo_name.bundle $CI_COMMIT_REF_NAME
+echo "Bundling repository at HEAD"
+git bundle create ../$repo_name.bundle HEAD
 cd -
 echo "Deleting repository $repo_path"
 rm -rf $repo_path
