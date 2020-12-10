136e312314620d303bc4e3c27473a3af672563a8
Zachariah Dzielinski <dzielinski_zachariah@bah.com>
2020-12-10 12:58:00 -0500

    CI test

--- scripts/deploy/01_deploy_bigbang.sh
+++ scripts/deploy/01_deploy_bigbang.sh
@@ -3,17 +3,6 @@ set -ex
 
 # Deploy flux and wait for it to be ready
 echo "Installing Flux"
-<<<<<<< HEAD
-kubectl apply -f https://repo1.dsop.io/platform-one/big-bang/apps/sandbox/fluxv2/-/raw/master/flux-system.yaml
-flux check
-
-# Deploy BigBang
-helm upgrade -i bigbang chart -n bigbang --create-namespace --set registryCredentials.username='robot$bigbang' --set registryCredentials.password=${REGISTRY1_PASSWORD} --set addons.argocd.enabled=true --set addons.authservice.enabled=true
-
-# Apply secrets kustomization pointing to current branch
-echo "Deploying secrets from branch ${CI_COMMIT_REF_NAME}"
-cat examples/complete/envs/dev/source-secrets.yaml | sed 's|master|'$CI_COMMIT_REF_NAME'|g' | kubectl apply -f -
-=======
 flux --version
 flux check --pre
 flux install
@@ -27,4 +16,3 @@ helm upgrade -i bigbang chart -n bigbang --create-namespace \
 ## Apply secrets kustomization pointing to current branch
 echo "Deploying secrets from the ${CI_COMMIT_REF_NAME} branch"
 cat tests/ci/shared-secrets.yaml | sed 's|master|'$CI_COMMIT_REF_NAME'|g' | kubectl apply -f -
->>>>>>> master
