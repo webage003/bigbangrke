2ae26cf99dffad8f1fb994ae8f17924f4e1461d4
Zachariah Dzielinski <dzielinski_zachariah@bah.com>
2020-12-11 12:03:36 -0500

    CI test

--- scripts/wait_core_pods.sh
+++ scripts/wait_core_pods.sh
@@ -2,5 +2,8 @@
 
 set -e
 
-for i in {1..10}; do; kubectl wait --for=condition=Ready -n kube-system $(kubectl get po -A -o name) &>/dev/null && break || sleep 5; done
+for i in {1..10}; do
+    kubectl wait --for=condition=Ready -n kube-system $(kubectl get po -A -o name) &>/dev/null && break || sleep 5
+done
+
 kubectl wait --for=condition=Ready -n kube-system $(kubectl get po -A -o name)
