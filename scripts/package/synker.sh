e6a343371e9298b24f212b98979eae927d08bde0
Josh Wolf <josh@joshwolf.dev>
2020-12-15 21:20:10 -0700

    testit

---
+++ scripts/package/synker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cat << EOF > "synker.yaml"
+transport:
+  registry:
+    hostname: localhost
+    port: 25000
+    osChoice: linux
+    disablePolicyChecks: true
+
+options:
+  # TODO: Turn this off for now until parallel syncing is more "production" ready
+  # NOTE: Finding some bugs (even with retry) in pushing images to the airgapped registry that trace back to paralell syncing
+  parallelSync: false
+  quiet: false
+  debug: info
+
+source:
+  authFile: /root/.docker/config.json
+  images:
+    - alpine:latest
+    - nginx:latest
+EOF
