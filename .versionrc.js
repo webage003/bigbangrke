f7c1f4bacc016aa7aefe8f587c21b194225349f4
Zachariah Dzielinski <dzielinski_zachariah@bah.com>
2021-01-19 18:08:17 -0700

    chore(release): 1.1.0

---
+++ .versionrc.js
@@ -0,0 +1,10 @@
+const standardVersionUpdaterYaml = require.resolve("standard-version-updater-yaml");
+ 
+module.exports = {
+  bumpFiles: [
+    {
+      filename: "chart/Chart.yaml",
+      updater: standardVersionUpdaterYaml
+    }
+  ]
+};
