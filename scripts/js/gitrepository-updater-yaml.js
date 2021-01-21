12a6fcec25d67a62e44c5e2da2d18280101a3ad0
Zachariah Dzielinski <dzielinski_zachariah@bah.com>
2021-01-20 19:08:36 -0700

    chore(release): 1.9.0

--- scripts/js/gitrepository-updater-yaml.js
+++ scripts/js/gitrepository-updater-yaml.js
@@ -2,6 +2,11 @@ const yaml = require("yaml");
 
 module.exports.readVersion = contents => yaml.parse(contents, "utf8").version;
 
+module.exports.readVersion = (contents) => {
+  const yamlFile = yaml.parse(contents, "utf8");
+  return yamlFile.spec.ref.tag;
+};
+
 module.exports.writeVersion = (contents, version) => {
   const yamlFile = yaml.parse(contents, "utf8");
   yamlFile.spec.ref.tag = version;
