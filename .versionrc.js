2c112f1372f185363a3457be6ccb8bc404f96d23
Zachariah Dzielinski <dzielinski_zachariah@bah.com>
2021-01-20 19:07:24 -0700

    chore(release): 1.7.0

--- .versionrc.js
+++ .versionrc.js
@@ -1,10 +1,15 @@
 const standardVersionUpdaterYaml = require.resolve("standard-version-updater-yaml");
+const gitrepositoryUpdaterYaml = require("scripts/js/gitrepository-updater-yaml.js");
 
 module.exports = {
   bumpFiles: [
     {
       filename: "chart/Chart.yaml",
       updater: standardVersionUpdaterYaml
+    },
+    {
+      filename: "base/gitrepository.yaml",
+      updater: gitrepositoryUpdaterYaml
     },
     {
       filename: "package.json",
