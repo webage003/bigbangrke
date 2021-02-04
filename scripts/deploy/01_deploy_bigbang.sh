fe8785218b7c8a0768c71250c51c2a0c3b4fd2df
Tunde Oladipupo <simplytunde@gmail.com>
2021-02-04 14:09:47 -0600

    ci: test script

--- scripts/deploy/01_deploy_bigbang.sh
+++ scripts/deploy/01_deploy_bigbang.sh
@@ -10,18 +10,15 @@ flux check --pre
 # create flux namespace
 kubectl create ns flux-system || true
 
-# TODO When changing the flux images to .mil this will need to chagne
 # delete flux private-registry secret
 kubectl delete secret private-registry -n flux-system || true
 
-if [[ -z "${AIRGAP}" ]]; then
 # create flux private-registry secret
 kubectl create secret docker-registry private-registry -n flux-system \
    --docker-server=registry1.dso.mil \
    --docker-username='robot$bigbang' \
    --docker-password=${REGISTRY1_PASSWORD} \
    --docker-email=bigbang@bigbang.dev || true
-fi
 
 # install flux
 kubectl apply -f ./scripts/deploy/flux.yaml
@@ -31,18 +28,10 @@ flux check
 
 # deploy BigBang using dev sized scaling
 echo "Installing BigBang"
-if [[ -z "${AIRGAP}" ]]; then
 helm upgrade -i bigbang chart -n bigbang --create-namespace \
 --set registryCredentials[0].username='robot$bigbang' --set registryCredentials[0].password=${REGISTRY1_PASSWORD} \
 --set registryCredentials[0].registry=registry1.dso.mil \
 -f tests/ci/k3d/values.yaml
-else
-kubectl set env deploy/source-controller -n flux-system HTTP_PROXY=http://proxy.dso.mil:8888
-kubectl set env deploy/source-controller -n flux-system HTTPS_PROXY=http://proxy.dso.mil:8888
-kubectl set env deploy/source-controller -n flux-system NO_PROXY=notification-controller,10.42.0.0/16,10.43.0.0/16
-helm upgrade -i bigbang chart -n bigbang --create-namespace \
--f tests/ci/k3d/values.yaml --set registryCredentials=null
-fi
 
 # apply secrets kustomization pointing to current branch
 echo "Deploying secrets from the ${CI_COMMIT_REF_NAME} branch"
