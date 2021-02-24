da0a802a193c4eeb25c6fa5cc5aab5e1368e60fc
Arun <asanna@vivsoft.io>
2021-02-23 20:30:41 -0500

    updated code

--- scripts/deploy/01_deploy_bigbang.sh
+++ scripts/deploy/01_deploy_bigbang.sh
@@ -16,7 +16,7 @@ kubectl delete secret private-registry -n flux-system || true
 # create flux private-registry secret
 kubectl create secret docker-registry private-registry -n flux-system \
    --docker-server=registry1.dso.mil \
-   --docker-username='robot$bigbang' \
+   --docker-username='robot$p1-dev' \
    --docker-password=${REGISTRY1_PASSWORD} \
    --docker-email=bigbang@bigbang.dev || true
 
@@ -29,15 +29,14 @@ flux check
 # deploy BigBang using dev sized scaling
 echo "Installing BigBang"
 helm upgrade -i bigbang chart -n bigbang --create-namespace \
---set registryCredentials[0].username='robot$bigbang' --set registryCredentials[0].password=${REGISTRY1_PASSWORD} \
+--set registryCredentials[0].username='robot$p1-dev' --set registryCredentials[0].password=${REGISTRY1_PASSWORD} \
 --set registryCredentials[0].registry=registry1.dso.mil \
--f tests/ci/k3d/values.yaml
-
-# apply secrets kustomization pointing to current branch
-echo "Deploying secrets from the ${CI_COMMIT_REF_NAME} branch"
-if [[ -z "${CI_COMMIT_TAG}" ]]; then
-  cat tests/ci/shared-secrets.yaml | sed 's|master|'$CI_COMMIT_REF_NAME'|g' | kubectl apply -f -
-else
-  # NOTE: $CI_COMMIT_REF_NAME = $CI_COMMIT_TAG when running on a tagged build
-  cat tests/ci/shared-secrets.yaml | sed 's|branch: master|tag: '$CI_COMMIT_REF_NAME'|g' | kubectl apply -f -
-fi
+-f /Users/thisiza/code/workshop/fortify/bigbang.yaml
+# # apply secrets kustomization pointing to current branch
+# echo "Deploying secrets from the ${CI_COMMIT_REF_NAME} branch"
+# if [[ -z "${CI_COMMIT_TAG}" ]]; then
+#   cat tests/ci/shared-secrets.yaml | sed 's|master|'$CI_COMMIT_REF_NAME'|g' | kubectl apply -f -
+# else
+#   # NOTE: $CI_COMMIT_REF_NAME = $CI_COMMIT_TAG when running on a tagged build
+#   cat tests/ci/shared-secrets.yaml | sed 's|branch: master|tag: '$CI_COMMIT_REF_NAME'|g' | kubectl apply -f -
+# fi
