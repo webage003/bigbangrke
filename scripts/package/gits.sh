9b5f3383f338af96547e500acb9c3ac07370a6b8
Tunde Oladipupo <simplytunde@gmail.com>
2021-03-02 09:21:55 -0600

    ci: updated package

--- scripts/package/gits.sh
+++ scripts/package/gits.sh
@@ -12,3 +12,7 @@ done
 yq r "chart/values.yaml" "addons.*.git.repo" | while IFS= read -r repo; do
     git -C repos/ clone --no-checkout $repo
 done
+
+if [ -n "${AIRGAP}" ]; then
+   git -C repos/ clone --branch ${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME} --no-checkout ${CI_REPOSITORY_URL}
+fi
