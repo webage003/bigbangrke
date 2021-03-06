fe4ad8f4e3bfe693fbec6e65955308f70a989046
kenna <kenna.ofoegbu@gmail.com>
2021-03-06 05:39:59 -0500

    test add other hardened containers

--- hack/sops-create.sh
+++ hack/sops-create.sh
@@ -3,7 +3,8 @@
 # This script will deploy install the bigbang-dev.asc private key into a sops-gpg secret in the bigbang namespace
 
 # Constants
-file=$(realpath `dirname "$0"`)/bigbang-dev.asc
+#file=$(realpath `dirname "$0"`)/bigbang-dev.asc
+file=bigbang-dev.asc
 ns=bigbang
 secret=sops-gpg
 key=bigbangkey
@@ -21,7 +22,7 @@ check_tool kubectl
 
 echo Installing SOPS secret from ${file} into ${ns}/${secret}...
 
-kubectl create namespace ${ns} 2> /dev/null
+#kubectl create namespace ${ns} 2> /dev/null
 kubectl create secret generic ${secret} --namespace=${ns} --from-file=${key}=${file}
 if [ $? -ne 0 ]; then echo ERROR: Secret creation failed!; exit 1; fi
 
