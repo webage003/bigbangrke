220d3f5e4d396a76cc9146aeaca35da1520d5b87
garcia.ryan <garcia.ryan@solute.us>
2021-03-15 15:20:37 -0600

    adding echo statements to cypress script

--- tests/bash/03_cypress.sh
+++ tests/bash/03_cypress.sh
@@ -8,6 +8,7 @@ mkdir -p cypress-tests/
 #Cloning core
 yq e '. | keys | .[] | ... comments=""' "tests/ci/k3d/values.yaml" | while IFS= read -r package; do
   if [[ "$(yq e ".${package}.enabled" "tests/ci/k3d/values.yaml")" == "true" ]]; then
+    echo "Cloning ${package} into cypress-tests"
     git -C cypress-tests/ clone -b $(yq e ".${package}.git.tag" "chart/values.yaml") $(yq e ".${package}.git.repo" "chart/values.yaml")
   fi
 done
@@ -16,13 +17,15 @@ done
 IFS=","
 for for package in $CI_MERGE_REQUEST_LABELS
 do
-    git -C cypress-tests/ clone -b $(yq e "addons.${package}.git.tag" "chart/values.yaml") $(yq e "addons.${package}.git.repo" "chart/values.yaml")
+  echo "Cloning ${package} into cypress-tests"
+  git -C cypress-tests/ clone -b $(yq e "addons.${package}.git.tag" "chart/values.yaml") $(yq e "addons.${package}.git.repo" "chart/values.yaml")
 done
 
 #Running Cypress tests
 for dir in cypress-tests/*/
 do
   if [ -f "${dir}tests/cypress.json" ]; then
+    echo "Running cypress tests in ${dir}"
     cypress run -w "${dir}"tests
   fi
 done
