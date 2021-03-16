08e3976f4e721500841904214c466082551143c9
garcia.ryan <garcia.ryan@solute.us>
2021-03-16 11:58:35 -0600

    Syntax fixing cloning addon packages

--- tests/bash/03_cypress.sh
+++ tests/bash/03_cypress.sh
@@ -25,12 +25,12 @@ for package in $CI_MERGE_REQUEST_LABELS; do
   echo "Cloning enabled add-ons"
   if [ "$(yq e ".addons.${package}.enabled" "tests/ci/k3d/values.yaml" 2>/dev/null)" != null ]; then
     #Checking for branch not tag
-    if [ "$(yq e ".${package}.git.tag" "chart/values.yaml")" != null ]; then
+    if [ "$(yq e ".addons.${package}.git.tag" "chart/values.yaml")" != null ]; then
       echo "Cloning ${package} into cypress-tests"
-      git -C cypress-tests/ clone -b $(yq e "addons.${package}.git.tag" "chart/values.yaml") $(yq e "addons.${package}.git.repo" "chart/values.yaml")
+      git -C cypress-tests/ clone -b $(yq e ".addons.${package}.git.tag" "chart/values.yaml") $(yq e ".addons.${package}.git.repo" "chart/values.yaml")
     else
       echo "Cloning ${package} into cypress-tests"
-      git -C cypress-tests/ clone -b $(yq e "addons.${package}.git.branch" "chart/values.yaml") $(yq e "addons.${package}.git.repo" "chart/values.yaml")
+      git -C cypress-tests/ clone -b $(yq e ".addons.${package}.git.branch" "chart/values.yaml") $(yq e ".addons.${package}.git.repo" "chart/values.yaml")
     fi
   fi
 done
