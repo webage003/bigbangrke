09c20eecab14327636fee33885a749836c8e51d3
garcia.ryan <garcia.ryan@solute.us>
2021-03-16 10:51:40 -0600

    Adding in syntax to check for branches in .git values

--- tests/bash/03_cypress.sh
+++ tests/bash/03_cypress.sh
@@ -8,8 +8,14 @@ mkdir -p cypress-tests/
 #Cloning core
 yq e '. | keys | .[] | ... comments=""' "tests/ci/k3d/values.yaml" | while IFS= read -r package; do
   if [[ "$(yq e ".${package}.enabled" "tests/ci/k3d/values.yaml")" == "true" ]]; then
-    echo "Cloning ${package} into cypress-tests"
-    git -C cypress-tests/ clone -b $(yq e ".${package}.git.tag" "chart/values.yaml") $(yq e ".${package}.git.repo" "chart/values.yaml")
+    #Checking for branch not tag
+    if [ "$(yq e ".${package}.git.tag" "chart/values.yaml")" != null ]; then
+      echo "Cloning ${package} into cypress-tests"
+      git -C cypress-tests/ clone -b $(yq e ".${package}.git.tag" "chart/values.yaml") $(yq e ".${package}.git.repo" "chart/values.yaml")
+    else
+      echo "Cloning ${package} into cypress-tests"
+      git -C cypress-tests/ clone -b $(yq e ".${package}.git.branch" "chart/values.yaml") $(yq e ".${package}.git.repo" "chart/values.yaml")
+    fi
   fi
 done
 
@@ -18,8 +24,13 @@ IFS=","
 for package in $CI_MERGE_REQUEST_LABELS
 do
   if [ "$(yq e ".addons.${package}.enabled" "tests/ci/k3d/values.yaml" 2>/dev/null)" == "false" ]; then
-    echo "Cloning ${package} into cypress-tests"
-    git -C cypress-tests/ clone -b $(yq e "addons.${package}.git.tag" "chart/values.yaml") $(yq e "addons.${package}.git.repo" "chart/values.yaml")
+    #Checking for branch not tag
+    if [ "$(yq e ".${package}.git.tag" "chart/values.yaml")" != null ]; then
+      echo "Cloning ${package} into cypress-tests"
+      git -C cypress-tests/ clone -b $(yq e "addons.${package}.git.tag" "chart/values.yaml") $(yq e "addons.${package}.git.repo" "chart/values.yaml")
+    else
+      echo "Cloning ${package} into cypress-tests"
+      git -C cypress-tests/ clone -b $(yq e "addons.${package}.git.branch" "chart/values.yaml") $(yq e "addons.${package}.git.repo" "chart/values.yaml")
   fi
 done
 
